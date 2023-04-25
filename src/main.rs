71ead86dd5c6cc5e0ec1d039b4aed48780fb5438
Nahua Kang <n.kang@opentalk.eu>
2023-04-25 17:43:08 +0200

    Create GameOver event for sending/reading to end the game

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ fn main() {
         .init_resource::<Score>()
         .init_resource::<StarSpawnTimer>()
         .init_resource::<EnemySpawnTimer>()
+        .add_event::<GameOver>()
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_player)
         .add_startup_system(spawn_enemies)
@@ -36,6 +37,7 @@ fn main() {
         .add_system(tick_enemy_spawn_timer)
         .add_system(spawn_enemies_over_time)
         .add_system(exit_game)
+        .add_system(handle_game_over)
         .run();
 }
 
@@ -79,6 +81,11 @@ impl Default for EnemySpawnTimer {
             timer: Timer::from_seconds(ENEMY_SPAWN_TIME, TimerMode::Repeating),
         }
     }
+}
+
+// GameOver event
+pub struct GameOver {
+    pub score: u32,
 }
 
 pub fn spawn_player(
@@ -293,10 +300,12 @@ pub fn confine_enemy_movement(
 
 pub fn enemy_hit_player(
     mut commands: Commands,
+    mut game_over_event_writer: EventWriter<GameOver>,
     mut player_query: Query<(Entity, &Transform), With<Player>>,
     enemy_query: Query<&Transform, With<Enemy>>,
-    audio: Res<Audio>,
     asset_server: Res<AssetServer>,
+    audio: Res<Audio>,
+    score: Res<Score>,
 ) {
     if let Ok((player_entity, player_transform)) = player_query.get_single_mut() {
         for enemy_transform in enemy_query.iter() {
@@ -311,6 +320,7 @@ pub fn enemy_hit_player(
                 let sound_effect = asset_server.load("audio/explosionCrunch_000.ogg");
                 audio.play(sound_effect);
                 commands.entity(player_entity).despawn();
+                game_over_event_writer.send(GameOver { score: score.value });
             }
         }
     }
@@ -412,5 +422,11 @@ pub fn exit_game(
 ) {
     if keyboard_input.just_pressed(KeyCode::Escape) {
         app_exit_event_writer.send(AppExit);
+    }
+}
+
+pub fn handle_game_over(mut game_over_event_reader: EventReader<GameOver>) {
+    for event in game_over_event_reader.iter() {
+        println!("Your final score is: {}", event.score);
     }
 }
