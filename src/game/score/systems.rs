2072fe6a47a0a3614c1f4836ebecc6deecc18cd9
Nahua Kang <kangnahua@gmail.com>
2023-04-25 23:44:48 +0200

    Use States for ScorePlugin

--- src/game/score/systems.rs
+++ src/game/score/systems.rs
@@ -4,6 +4,14 @@ use crate::events::GameOver;
 
 use super::resources::{HighScores, Score};
 
+pub fn insert_score(mut commands: Commands) {
+    commands.insert_resource(Score::default());
+}
+
+pub fn remove_score(mut commands: Commands) {
+    commands.remove_resource::<Score>();
+}
+
 pub fn update_score(score: Res<Score>) {
     if score.is_changed() {
         println!("Score: {}", score.value);
--- src/systems.rs
+++ src/systems.rs
@@ -46,8 +46,9 @@ pub fn exit_game(
     }
 }
 
-pub fn handle_game_over(mut game_over_event_reader: EventReader<GameOver>) {
+pub fn handle_game_over(mut commands: Commands, mut game_over_event_reader: EventReader<GameOver>) {
     for event in game_over_event_reader.iter() {
         println!("Your final score is: {}", event.score);
+        commands.insert_resource(NextState(Some(AppState::GameOver)));
     }
 }
