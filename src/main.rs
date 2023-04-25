fdedefd0a6c87ad522b82ef4222f0b225a8851d0
Nahua Kang <kangnahua@gmail.com>
2023-04-25 21:55:12 +0200

    Use GamePlugin and refactor

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,18 @@
-pub mod enemy;
 pub mod events;
-pub mod player;
-pub mod score;
-pub mod star;
+mod game;
+mod main_menu;
 pub mod systems;
 
 use bevy::prelude::*;
-use enemy::EnemyPlugin;
-use events::*;
-use player::PlayerPlugin;
-use score::ScorePlugin;
-use star::StarPlugin;
+use game::*;
+use main_menu::*;
 use systems::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_event::<GameOver>()
-        .add_plugin(EnemyPlugin)
-        .add_plugin(PlayerPlugin)
-        .add_plugin(ScorePlugin)
-        .add_plugin(StarPlugin)
+        .add_plugin(MainMenuPlugin)
+        .add_plugin(GamePlugin)
         .add_startup_system(spawn_camera)
         .add_system(exit_game)
         .add_system(handle_game_over)
