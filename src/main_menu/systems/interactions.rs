d1e4472d1d084a6ed7112f6cbfd66bbfedd96901
Nahua Kang <kangnahua@gmail.com>
2023-04-26 21:39:52 +0200

    Add MainMenu UI for interaction

---
+++ src/main_menu/systems/interactions.rs
@@ -0,0 +1,55 @@
+use bevy::{app::AppExit, prelude::*};
+
+use crate::{
+    main_menu::{
+        components::*,
+        styles::{HOVERED_BUTTON_COLOR, NORMAL_BUTTON_COLOR, PRESSED_BUTTON_COLOR},
+    },
+    AppState,
+};
+
+pub fn interact_with_play_button(
+    mut play_button_query: Query<
+        (&Interaction, &mut BackgroundColor),
+        (Changed<Interaction>, With<PlayButton>),
+    >,
+    mut app_state_next_state: ResMut<NextState<AppState>>,
+) {
+    if let Ok((interaction, mut background_color)) = play_button_query.get_single_mut() {
+        match *interaction {
+            Interaction::Clicked => {
+                *background_color = PRESSED_BUTTON_COLOR.into();
+                app_state_next_state.set(AppState::Game);
+            }
+            Interaction::Hovered => {
+                *background_color = HOVERED_BUTTON_COLOR.into();
+            }
+            Interaction::None => {
+                *background_color = NORMAL_BUTTON_COLOR.into();
+            }
+        }
+    }
+}
+
+pub fn interact_with_quit_button(
+    mut app_exit_event_writer: EventWriter<AppExit>,
+    mut quit_button_query: Query<
+        (&Interaction, &mut BackgroundColor),
+        (Changed<Interaction>, With<QuitButton>),
+    >,
+) {
+    if let Ok((interaction, mut background_color)) = quit_button_query.get_single_mut() {
+        match *interaction {
+            Interaction::Clicked => {
+                *background_color = PRESSED_BUTTON_COLOR.into();
+                app_exit_event_writer.send(AppExit);
+            }
+            Interaction::Hovered => {
+                *background_color = HOVERED_BUTTON_COLOR.into();
+            }
+            Interaction::None => {
+                *background_color = NORMAL_BUTTON_COLOR.into();
+            }
+        }
+    }
+}
